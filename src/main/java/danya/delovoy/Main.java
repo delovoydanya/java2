e3f2e83a79f4909afe11f6e6b9a9badedf618f8c
DPSemenov <daniilsemenov779@gmail.com>
2025-07-09 01:26:45 +0300

    add test

--- src/main/java/danya/delovoy/Main.java
+++ src/main/java/danya/delovoy/Main.java
@@ -14,4 +14,8 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+        public int summ(int first, int second) {
+            return first + second;
+        }
+
 }
